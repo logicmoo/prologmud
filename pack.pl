62be9f86ad4c6101c735300cc9a461473fbb7dd3
logicmoo <logicmoo@gmail.com>
2020-10-18 06:59:52 -0700

    version_bumps3

--- pack.pl
+++ pack.pl
@@ -4,14 +4,9 @@ title('Online text adventure game - MUD Server').
 keywords([game,ai]).
 
 author('Douglas R. Miles','logicmoo@gmail.com').
-author('Douglas Miles', 'http://www.linkedin.com/in/logicmoo').
 packager('TeamSPoon', 'https://github.com/TeamSPoon/').
 maintainer('TeamSPoon', 'https://github.com/TeamSPoon/').
 home('https://github.com/TeamSPoon/prologmud.git').
 download( 'https://github.com/TeamSPoon/prologmud/release/*.zip').
 requires(logicmoo_base).
-
-
-
-
 
